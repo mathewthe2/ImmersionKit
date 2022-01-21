e87104875bb1345304058074eaba51df267d6f91
Mathew Chan <mchan6466@gmail.com>
2022-01-21 09:52:12 +0800

    directly open link without app window

--- __init__.py
+++ __init__.py
@@ -3,22 +3,30 @@ from .server.config import config
 from .server.run_server import Server
 from aqt.webview import AnkiWebView
 from aqt import mw
+from aqt.utils import openLink
 from aqt.qt import *
 
 s = Server('0.0.0.0', 5006, dev_mode=config['dev_mode'])
 
+WINDOW_DISPLAY = False
+CLIENT_URL = "http://localhost:5006/search.html"
+
 class ImmersionKitWebView(AnkiWebView):
     def __init__(self):
         AnkiWebView.__init__(self, title="Immersion Kit")
-        url = QUrl("http://localhost:5006/search.html")
+        url = QUrl(CLIENT_URL)
         # self.set_open_links_externally(False)
         self.load_url(url)
 
 def showApp():
-    mw.immersionKitApp = ImmersionKitWebView()
-    mw.immersionKitApp.show()
-    mw.immersionKitApp.setFocus()
-    mw.immersionKitApp.activateWindow()
+    if WINDOW_DISPLAY:
+        mw.immersionKitApp = ImmersionKitWebView()
+        mw.immersionKitApp.show()
+        mw.immersionKitApp.setFocus()
+        mw.immersionKitApp.activateWindow()
+    else:
+        openLink(QUrl(CLIENT_URL))
+
 
 action = QAction("Immersion Kit Web", mw)
 
